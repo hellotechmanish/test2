aec69d76719c0e26bd7636d76240e91c6489fee4
Manish kumar <107772484+hellotechmanish@users.noreply.github.com>
2024-12-25 10:38:37 +0530

    Create addnum.c
    add two number with use third veriable program

---
+++ addnum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+     int a,b,c;
+     printf("hello kumar_manish70\n");
+     printf("write a num a,b\n");
+     scanf("%d%d",&a,&b);
+    c=a+b;
+        printf("%d",c);
+    
+  
+    return 0;
+}
